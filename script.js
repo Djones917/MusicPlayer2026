813e892f64239fbcd4fee320c66afdcf33814441
Derek <nightwing917@hotmail.com>
2026-03-12 19:02:29 -0600

    grabbed audio element

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const musicContainer = document.getElementById('music-container');
 const playBTN = document.getElementById('play');
 const prevBTN = document.getElementById('prev');
 const nextBTN = document.getElementById('next');
+const audio = document.getElementById('audio');
