2d4d8a973833dcbf9087f491a34ff7282bb86f59
Derek <nightwing917@hotmail.com>
2026-03-13 19:09:53 -0600

    grabbed progressContainer

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const prevBTN = document.getElementById('prev');
 const nextBTN = document.getElementById('next');
 const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('music-container');
+
 
