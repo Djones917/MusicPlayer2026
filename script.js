ed03470b9ed56f1a2558dbed24babdf3b85745b2
Derek <nightwing917@hotmail.com>
2026-06-06 17:32:29 -0600

    fixed playBTN

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const musicContainer = document.getElementById('music-container');
-const playBTN = document.getElementById('play');
+const playBtn = document.getElementById('play');
 const prevBTN = document.getElementById('prev');
 const nextBTN = document.getElementById('next');
 const audio = document.getElementById('audio');
